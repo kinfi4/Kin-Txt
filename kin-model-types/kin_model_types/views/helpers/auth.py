bb16232f5c2ba7e1afb68528d2e047b572197f25
kinfi4 <ilyamakarov223@gmail.com>
2023-08-30 08:40:13 +0300

    [MODEL TYPES] Report generation validated.

--- kin-model-types/kin_model_types/views/helpers/auth.py
+++ kin-model-types/kin_model_types/views/helpers/auth.py
@@ -1,7 +1,7 @@
 from fastapi import Header, HTTPException, status, Request
 
 from kin_news_core.auth import decode_jwt_token, decode_kin_token
-from kin_news_core.constants import KIN_TOKEN_PREFIX
+from kin_news_core.constants import KIN_TOKEN_PREFIX, USERNAME_HEADER
 from kin_news_core.exceptions import AuthenticationFailedError
 
 from kin_model_types.domain.entities import User
@@ -10,6 +10,9 @@ from kin_model_types.settings import Settings
 
 INTERNAL_URLS = [
     "/api/model-types/v1/model",
+    "/api/model-types/v1/blobs/get-model-binaries",
+    "/api/model-types/v1/blobs/get-tokenizer-binaries",
+    "/api/model-types/v1/visualization-template",
 ]
 
 
@@ -17,7 +20,7 @@ def get_current_user(request: Request, authorization: str = Header(None)):
     if not authorization:
         raise HTTPException(status.HTTP_401_UNAUTHORIZED)
 
-    if authorization.startswith(KIN_TOKEN_PREFIX) and request.url.path in INTERNAL_URLS:
+    if authorization.startswith(KIN_TOKEN_PREFIX) and any([internal_path in request.url.path for internal_path in INTERNAL_URLS]):
         if request.method != "GET":
             raise HTTPException(status.HTTP_405_METHOD_NOT_ALLOWED)
 
@@ -29,7 +32,8 @@ def get_current_user(request: Request, authorization: str = Header(None)):
         if decoded_token != Settings().kin_token:
             raise HTTPException(status.HTTP_403_FORBIDDEN)
 
-        return User(username="", internal_user=True)
+        requester_username = request.headers.get(USERNAME_HEADER, "")
+        return User(username=requester_username, internal_user=True)
 
     try:
         decoded_username = decode_jwt_token(authorization)
